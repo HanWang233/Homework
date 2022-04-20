e758ff1f53a4e898f397268de33c6a3e22301949
HanWang <han.wang@enlighten.finance>
2022-04-20 21:52:37 +0800

    阅读ERC20, ERC721, ERC1155标准，并将标准内function和event转换成signature

---
+++ Level2/2/Signature.js
@@ -0,0 +1,24 @@
+var Web3 = require("web3")
+
+const web3 = new Web3("https://cloudflare-eth.com")
+
+var Function = [
+    "name()",
+    "symbol()",
+    "decimals()",
+    "totalSupply()",
+    "balanceOf(address)",
+    "transfer(address,uint256)",
+    "transferFrom(address,address,uint256)",
+    "approve(address, uint256)",
+    "allowance(address,address)"];
+for (let i = 0; i < Function.length; i++) {
+    console.log('The function name is:' + Function[i] + ', ' + 'convert result is: ' + web3.eth.abi.encodeFunctionSignature(Function[i]));
+}
+
+var Event = [
+    "Transfer(address,address,uint256)",
+    "Approval(address,address,uint256)"];
+for (let i = 0; i < Event.length; i++) {
+    console.log('The event name is:' + Event[i] + ', ' + 'convert result is: ' + web3.eth.abi.encodeFunctionSignature(Event[i]));
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Homework
+
+### Level 2
+- [阅读ERC20, ERC721, ERC1155标准，并将标准内function和event转换成signature](https://github.com/HanWang233/Homework/tree/master/Level2/2)
+  ![](Level2/2/Signature.png)
